109f5782f59baa850391e8bb19117311f38c1f5a
Muhammed Jasil <mohammedjass007@gmail.com>
2025-09-11 21:03:02 +0000

    scriipt.sh some changes

--- stage-1/exercise2/script.sh
+++ stage-1/exercise2/script.sh
@@ -9,3 +9,9 @@
  cd "/mnt/d/nitc curriculum/sem5/compiler design lab/xsm_expl"
 
  ./xsm -l ../files/stage-1/library.lib -e ../files/stage-1/exercise2/target_file.xsm
+
+ cd -
+rm y.tab.c
+rm y.tab.h
+rm lex.yy.c
+rm exe
